007423a883d4292de7416a9a98b2bf30f60b2236
章鑫 <zhangx511@chinaunicom.cn>
2019-08-15 17:13:19 +0800

    eureka_server update 20190815

--- src/main/java/com/imooc/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/imooc/eurekaserver/EurekaServerApplication.java
@@ -1,15 +1,24 @@
 package com.imooc.eurekaserver;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author: 章鑫
+ * @Email: zhangx511@chinaunicom.cn
+ * @Project_name：eurekaserver
+ * @Name: EurekaServerApplication
+ * @date: 2019-08-14 11:28
+ * @Description:
+ **/
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaserverApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(EurekaserverApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
